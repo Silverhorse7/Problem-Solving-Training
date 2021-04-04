3bfca7fd34cd9d924fb128fce9d961580445a581
YosefMahmoud <yosefone212@gmail.com>
2021-04-04 13:12:00 +0200

    Add solution for problem `A - Yet Another Two Integers Problem` Link: https://codeforces.com/contest/1409/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1409/submission/111994348

--- codeforces/1409/A.cpp
+++ codeforces/1409/A.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
-using namespace std;
-///**************************** M a c r o ****************************///
+			using namespace std;
+ 
+			///**************************** M a c r o ****************************///
 #define fi(i,n) for(int x = 0 ; x<n;x++) //to write 2d arrays faster ,generally arrays use
 #define ll long long
 #define Speedo ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
@@ -22,139 +23,75 @@ using namespace std;
 #define lcm(a,b) (a*(b/gcd(a,b)))
 #define test   cout<<" We Fucked up Here Boss_________________"<<endl
 #define ezi(lblb) int lblb; cin>>lblb
-#define ezll(lblb) ll lblb; cin>>lblb		
-#define pii pair<int,int>pair
-//#define int ll
-#define star *
+#define ezll(lblb) ll lblb; cin>>lblb
 ///**************************** C o n s t ****************************///
-const double PI = acos(-1);
-const double eps = 1e-9;
-const int inf = 2000000000; //9 zeros
-const ll infLL = 9000000000000000000;
-const int lazy = int('a' - 'A');
-const ll N = 1e7 + 1;
-string alphabet = "abcdefghijklmnopqrstuvwxyz";
-ll sum = 0, final = 0, result = 0;
+		const double PI = acos(-1);
+		const double eps = 1e-9;
+		const int inf = 2000000000; //9 zeros
+		const ll infLL = 9000000000000000000;
+		const int lazy = int('a' - 'A');
+		const ll N = 1e7 + 1;
+		string alphabet = "abcdefghijklmnopqrstuvwxyz";
+		ll sum = 0, final = 0, result = 0;
 #define lmao(s,arr) for(int i =  0 ; i <s.size();i++){arr[i]=s[i];}
-#define allout(first) for(auto ele : first) {cout<<ele<<" ";}cout<<endl;
-#define f(i,n) for(int i = 0 ; i<n;i++)
-#define ceil(x) static_cast<long long>(ceil(x))
-#define float(x) static_cast<long long>(float(x))
-#define round(x) static_cast<long long>(round(x))
-///**************************** F U N C T I O N S  ****************************///
-//Comment Code Block Ctrl + K + C / Ctrl + K + U-->remove comment
-template <class T = int>
-T scan() {
-	T res;
-	std::cin >> res;
-	return res;
+		///**************************** F U N C T I O N S  ****************************///
+		//algorithm 
+		//	A		Hello
+		//	B		Codeforces
+		// B.FIND(A[I])==FALSE ? -->A.ERASE(A[I]); :
+		ull MOD=1000000007;
+ll fastPower(ll b, ll p) {
+    if (p == 0)
+        return 1;
+    ll ans = fastPower(b, p / 2);
+    ans = (ans * ans);
+    if (p % 2 != 0)
+        ans = (ans * b);
+    return ans;
 }
-int invertBits(int num)
-{
-	int x = log2(num) + 1;
-	for (int i = 0; i < x; i++)
-		num = (num ^ (1 << i));
-	return num;
-}
-bool correctbracketsequence(string s)
-{
-	stack<char>stk;
-	for (int i = 0; i < s.size(); i++)
-	{
-		if (s[i] == '(')
+	template<class T> T power(T N, T P) { return (P == 0) ? 1 : N * power(N, P - 1); }
+		bool isPalindrome(string s)
 		{
-			stk.push(s[i]);
-		}
-		else {
-			if (stk.size() > 0)
+			string lol = s;
+			reverse(lol.begin(), lol.end());
+			if (s == lol)
 			{
-				stk.pop();
+				return true;
 			}
-			else {
+			else
+			{
 				return false;
 			}
 		}
-	}
-	if (stk.size() > 0)return false;
-	return true;
-}
-void checklater()//that bitmasks problem
-{
-	string s;
-	cin >> s;
-	if (count(all(s), 'A') == count(all(s), 'B') and count(all(s), 'C') % 2 != 0)
-	{
-		cout << "YES" << endl; return;
-	}
-	int koko = count(all(s), 'A');
-	int lolo = count(all(s), 'B');
-	if (koko < lolo)
-	{
-		for (int i = 0; i < s.size(); i++)
+			void it_is_me_dio_hahahahahaha()//Problem I , go back to it ,acmascis one
 		{
-			if (s[i] == 'C')s[i] = 'A';
+			ll lol,lmao;
+			cin>>lol>>lmao;
+			ll a = max(lol,lmao);
+			ll b= min(lol,lmao);
+			ll diff = a-b;
+			ll cnt=0;
+			if(diff%10==0)
+			{
+				cnt+=diff/10;
+			}
+			else{
+				cnt+=diff/10;
+				cnt++;
+			}
+			cout<<cnt<<endl;
 		}
-	}
-	if (koko > lolo)
-	{
-		for (int i = 0; i < s.size(); i++)
+		int main()
 		{
-			if (s[i] == 'C')s[i] = 'B';
+			Speedo;
+			int t;
+			t = 1;
+			
+			cin >> t;
+			while (t--)
+			{
+				it_is_me_dio_hahahahahaha();
+			}	
 		}
-	}
-	// first one is (
-	char kekw = s[0];
-	for (int i = 0; i < s.size(); i++)
-	{
-		if (s[i] == kekw)s[i] = '(';
-		else {
-			s[i] == ')';
-		}
-	}
-	if (correctbracketsequence(s))
-	{
-		cout << "YES" << endl;
-	}
-	else {
-		cout << "NO" << endl;
-	}
-}
-void it_was_me_dio_hahahahahaha()// if u need precision with ceil
-// remove the ceil macro
-{
-	ll a, b;
-	cin >> a >> b;
-	if (b<a)
-	{
-		swap(a, b);
-	}
-	ll diff = b - a;
-	if (diff % 10 == 0)
-	{
-		cout << diff / 10 << endl;
-	}
-	else {
-		cout << (diff / 10) + 1 << endl;
-	}
-}
-int main()
-{
-	Speedo;
-	int t;
-	t = 1;
-	cin >> t;
-	while(t--)
-	{
-		it_was_me_dio_hahahahahaha();
-	}
-}
-// static (data type) by3'aly al data type mostmr 
-/**/
-/* stuff you should look for
-	* int overflow, array bounds
-	* special cases (n=1?)
-	* do smth instead of nothing and stay organized
-	* WRITE STUFF DOWN
-	* DON'T GET STUCK ON ONE APPROACH
-	* DON"T GET STUCK ON ONE PROBLEM
-*/
+		// static (data type) by3'aly al data type mostmr 
+		/**/
