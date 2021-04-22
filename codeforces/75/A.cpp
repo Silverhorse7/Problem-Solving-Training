1e0be955c0b7e4a559f44b0dde5566ef983a4eda
YosefMahmoud <yosefone212@gmail.com>
2021-04-22 14:34:00 +0200

    Add solution for problem `A - Life Without Zeros` Link: https://codeforces.com/contest/75/problem/A Tags: implementation, *1000 Ref: https://codeforces.com/contest/75/submission/113883695

---
+++ codeforces/75/A.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+using namespace std;
+///**************************** M a c r o ****************************///
+#define fi(i,n) for(int x = 0 ; x<n;x++) //to write 2d arrays faster ,generally arrays use
+#define ll long long
+#define Speedo ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define f(i,m,n) for(int i=m; i<n; i++)
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define print(arr,size) for(int i = 0; i<size;i++){cout<<arr[i]<<" ";}cout<<endl;
+#define news(f) int f; cin>>f;
+#define endl          '\n'
+#define return0 return 0
+#define Endl endl
+#define ull unsigned long long
+#define ssort(arr,n) sort(arr,arr+n)
+#define elif else if 
+#define lo(f) if (f>='a' && f<='z')
+#define up(f) if (f>='A' && f<='Z')
+#define pff(a) a.size()
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*(b/gcd(a,b)))
+#define test   cout<<" We Fucked up Here Boss_________________"<<endl
+#define ezi(lblb) int lblb; cin>>lblb
+#define ezll(lblb) ll lblb; cin>>lblb		
+#define pii pair<int,int>pair
+//#define int ll
+#define star *
+///**************************** C o n s t ****************************///
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000; //9 zeros
+const ll infLL = 9000000000000000000;
+const int lazy = int('a' - 'A');
+const ll N = 1e7 + 1;
+string alphabet = "abcdefghijklmnopqrstuvwxyz";
+ll sum = 0, final = 0, result = 0;
+#define lmao(s,arr) for(int i =  0 ; i <s.size();i++){arr[i]=s[i];}
+#define allout(first) for(auto ele : first) {cout<<ele<<" ";}cout<<endl;
+#define f(i,n) for(int i = 0 ; i<n;i++)
+///**************************** F U N C T I O N S  ****************************///
+//Comment Code Block Ctrl + K + C / Ctrl + K + U-->remove comment
+template <class T = int>
+T scan() {
+	T res;
+	std::cin >> res;
+	return res;
+}
+bool checkperfectsquare(int n)
+{
+	if (ceil((double)sqrt(n)) == floor((double)sqrt(n))) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+int invertBits(int num)
+{
+	int x = log2(num) + 1;
+	for (int i = 0; i < x; i++)
+		num = (num ^ (1 << i));
+	return num;
+}
+void it_was_me_dio_hahahahahaha()
+{
+	string a1;
+	string b1;
+	cin >> a1 >> b1;
+	ll copyc = stoull(a1) + stoull(b1);
+	string c = to_string(copyc);
+	string a = "";
+	string b = "";
+	for (int i = 0; i < a1.size(); i++)
+	{
+		if (a1[i] != '0')a += a1[i];
+	}
+	for (int i = 0; i < b1.size(); i++)
+	{
+		if (b1[i] != '0')b += b1[i];
+	}
+	string koko = "";
+	for (int i = 0; i < c.size(); i++)
+	{
+		if (c[i] != '0')koko += c[i];
+	}
+	if (stoull(a) + stoull(b) == stoull(koko))
+	{
+		cout << "YES" << endl;
+	}
+	else {
+		cout << "NO" << endl;
+	}
+}
+int main()
+{
+	Speedo;
+	int t;
+	t = 1;
+	//cin >> t;
+	while(t--)
+	{
+		it_was_me_dio_hahahahahaha();
+	}
+}
+// static (data type) by3'aly al data type mostmr 
+/**/
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* do smth instead of nothing and stay organized
+	* WRITE STUFF DOWN
+	* DON'T GET STUCK ON ONE APPROACH
+	* DON"T GET STUCK ON ONE PROBLEM
+*/
