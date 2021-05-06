86ebfceca9466cb29c0804ec7f00e4f33bc048ef
YosefMahmoud <yosefone212@gmail.com>
2021-05-06 14:08:00 +0200

    Add solution for problem `A - Domino piling` Link: https://codeforces.com/contest/50/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/50/submission/115415089

---
+++ codeforces/50/A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+#define ll long long
+#define Speedo ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define endl       '\n'
+#define return0 return 0
+#define Endl endl
+#define ull unsigned long long
+#define elif else if 
+#define test   cout<<" We Fucked up Here Boss_________________"<<endl
+#define allout(first) for(auto ele : first) {cout<<ele<<" ";}cout<<endl;
+void solve()
+{
+    double a, b;
+    cin >> a >> b;
+    a /= 2;
+    cout << int(a * b) << endl;
+}
+int main()
+{
+    Speedo;
+    int t = 1;
+   // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
