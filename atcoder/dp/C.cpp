c15299aae733c8769f5b39f3851d1d8751929feb
Yosef Mahmoud <yosefone212@gmail.com>
2021-07-19 14:17:00 +0200

    Add solution for problem `C - Vacation` Link: https://atcoder.jp/contests/dp/tasks/dp_c Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/24393732

--- atcoder/dp/C.cpp
+++ atcoder/dp/C.cpp
@@ -1,18 +1,78 @@
-#include<bits/stdc++.h>
+//#pragma GCC optimize("Ofast")
+//#pragma GCC optimize ("unroll-loops")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <bits/stdc++.h>
+
 using namespace std;
-int dp[300000][3];
-signed main() {
+#define ll long long
+#define Silver_is_better_than_Gold ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define endl       '\n'
+#define return0 return 0
+#define Endl endl
+#define ull unsigned long long
+#define elif else if
+#define test   cout<<" We Droped up Here Boss _________________ "<<endl
+#define allout(v) for(auto ele : v){cout<<ele<<" ";}cout<<endl
+#define mk make_pair
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pcc pair<char,char>
+#define vi vector<int>
+#define vl vector<ll>
+#define vvi vector<vector<int>>
+#define vvl vector<vector<ll>>
+#define txt freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+#define ld long double
+#define vb vector<bool>
+#define getlost {cout<<"NO"<<endl;return ;}
+#define getgud {cout<<"YES"<<endl;return ;}
+#define tests(x) for(int i =  1 ; i<=x;i++)
+#define inp(v,n) for(int i = 0 ;  i<n;i++)cin>>v[i];
+#define outp(v) for(auto ele : v){        cout << ele.first << " " << ele.second << " ";}cout<<endl
+const ll mod = 1e9 + 7;
+#define summ(v,sum) for(auto ele : v)sum+=ele;
+#define inti int i
+#define sz(x) ((int) (x).size())
+#define dups(divs)     divs.erase(unique(divs.begin(), divs.end()), divs.end());
+#define fo(i,a,b) for(int i = a ; i<=b;i++)
+#define outq(q)     queue<int>qq = q;while (qq.size()) { cout << qq.front() << " "; qq.pop();};
+#define sttk(x) static_cast<ll>(x)
+#define re register
+#define intj int j
+#define debug cout<<"Output is  :  ";
+#define debug2 cout<<"End of the output______________________"<<endl;
     int n;
-    cin>>n;
-    vector<vector<int>>v(n,vector<int>(3));
-    for(int i = 0 ; i <n;i++)for(int j = 0  ; j <3;j++)cin>>v[i][j];
-    dp[0][0]=v[0][0];
-    dp[0][1]=v[0][1];
-    dp[0][2]=v[0][2];
-    for(int i =1 ; i <n;i++){
-        dp[i][0]=max(dp[i-1][1],dp[i-1][2])+v[i][0];
-        dp[i][1]=max(dp[i-1][0],dp[i-1][2])+v[i][1];
-        dp[i][2]=max(dp[i-1][0],dp[i-1][1])+v[i][2];
+void solve() {
+    cin >> n;
+    vector < vector<int>>arr(n, vector<int>(3));
+    for (auto &ele : arr) {
+        for (auto &el : ele) {
+            cin >> el;
+        }
     }
-    cout<<max({dp[n-1][0],dp[n-1][1],dp[n-1][2]});
+    int dp[n][3]; 
+    memset(dp, 0, sizeof(dp));
+    dp[0][0] = arr[0][0];
+    dp[0][1] = arr[0][1];
+    dp[0][2] = arr[0][2];
+    for (int i = 1; i < n; i++) {
+        dp[i][0] = arr[i][0] + max(dp[i - 1][1], dp[i - 1][2]);
+        dp[i][1] = arr[i][1] + max(dp[i - 1][0], dp[i - 1][2]);
+        dp[i][2] = arr[i][2] + max(dp[i - 1][0], dp[i - 1][1]);
+    }
+    cout << max({ dp[n - 1][0],dp[n - 1][1],dp[n - 1][2] });
+}
+int main() {
+    Silver_is_better_than_Gold;
+    // For getting input from input.txt file
+    //freopen("mosalah.in", "r", stdin);
+    int t = 1;
+    //  cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    //-
 }
