cf4909ad8757a49a773cf79bdfd9d48467fdc012
Yosef Mahmoud <yosefone212@gmail.com>
2021-07-19 15:20:00 +0200

    Add solution for problem `D - Knapsack 1` Link: https://atcoder.jp/contests/dp/tasks/dp_d Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/24395061

--- atcoder/dp/D.cpp
+++ atcoder/dp/D.cpp
@@ -1,29 +1,94 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC optimize ("unroll-loops")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
 #include <bits/stdc++.h>
 
 using namespace std;
 #define ll long long
-using namespace std;
-#define fast {ios_base::sync_with_stdio();}
-#define inp(v) for(auto &i : v)cin>>i;
-#define T int T;cin>>T;while(T--)
+#define Silver_is_better_than_Gold ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define endl       '\n'
+#define return0 return 0
+#define Endl endl
+#define ull unsigned long long
+#define elif else if
+#define test   cout<<" We Droped up Here Boss _________________ "<<endl
+#define allout(v) for(auto ele : v){cout<<ele<<" ";}cout<<endl
+#define mk make_pair
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pcc pair<char,char>
+#define vi vector<int>
+#define vl vector<ll>
+#define vvi vector<vector<int>>
+#define vvl vector<vector<ll>>
+#define txt freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
 #define ld long double
-#define yes {cout<<"YES"<<endl;return;};
-#define no {cout<<"NO"<<endl;return;};
-#define pow(x, y) ll(pow(x,y))
-#define all(v) v.begin(),v.end()
-ll dp[109][100009];
-pair<ll, ll> v[109];
-
-signed main() {
-    int n, w;
-    cin >> n >> w;
-    for (int i = 0; i < n; i++)cin >> v[i].first >> v[i].second;
-    for (int i = 0; i <= w; i++) {
-        dp[0][i] = (i >= v[0].first ? v[0].second : 0);
+#define vb vector<bool>
+#define getlost {cout<<"NO"<<endl;return ;}
+#define getgud {cout<<"YES"<<endl;return ;}
+#define tests(x) for(int i =  1 ; i<=x;i++)
+#define inp(v,n) for(int i = 0 ;  i<n;i++)cin>>v[i];
+#define outp(v) for(auto ele : v){        cout << ele.first << " " << ele.second << " ";}cout<<endl
+const ll mod = 1e9 + 7;
+#define summ(v,sum) for(auto ele : v)sum+=ele;
+#define inti int i
+#define sz(x) ((int) (x).size())
+#define dups(divs)     divs.erase(unique(divs.begin(), divs.end()), divs.end());
+#define fo(i,a,b) for(int i = a ; i<=b;i++)
+#define outq(q)     queue<int>qq = q;while (qq.size()) { cout << qq.front() << " "; qq.pop();};
+#define sttk(x) static_cast<ll>(x)
+#define re register
+#define intj int j
+#define debug cout<<"Output is  :  ";
+#define debug2 cout<<"End of the output______________________"<<endl;
+    ll dp[100][100000];
+    bool valid(int x, int y, int w) {
+        return (x >= 0 and y >= 0 and x < w and y < w);
     }
-    for (int i = 1; i < n; i++)
-        for (int j = 1; j <= w; j++)
-            dp[i][j] = max(dp[i - 1][j], (j - v[i].first >= 0 ? dp[i - 1][j - v[i].first] + v[i].second : 0));
+void solve() {
+    int n;
+    cin >> n;
+    int w; 
+    cin >> w;
+    vector<pair<int, int>>pr(n);
+    for (auto& ele : pr) {
+        cin >> ele.first;
+        cin >> ele.second;
+    }
+    for (int i = 0; i <= w; i++) {
+        if (i >= pr[0].first) {
+            dp[0][i] = pr[0].second;
+        }
+        else {
+            dp[0][i] = 0;
+        }
+    }
+    for (inti = 0; i < n; i++) {
+        dp[i][0] = 0;
+    }
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j <= w; j++) {
+            ll mn = dp[i - 1][j];
+            if (valid(i - 1, j - pr[i].first, w))
+                mn = max(mn, pr[i].second + dp[i - 1][j - pr[i].first]);
+            
+            //dp[i][j]=max(dp[i-1][j],pr[i-1].first+dp[i-1][j-pr[j-1].first)
+            dp[i][j] = mn;
+        }
+    }
+    cout << dp[n - 1][w] << endl;
+}
+int main() {
+    Silver_is_better_than_Gold;
+    // For getting input from input.txt file
+    //freopen("mosalah.in", "r", stdin);
+    int t = 1;
+    //  cin >> t;
+    while (t--) {
+        solve();
+    }
 
-    cout << dp[n - 1][w];
+    //-
 }
