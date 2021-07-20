4a969c4edc859e426ce332368f5ca5aad29472d1
Yosef Mahmoud <yosefone212@gmail.com>
2021-07-20 09:10:00 +0200

    Add solution for problem `H - Grid 1` Link: https://atcoder.jp/contests/dp/tasks/dp_h Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/24405089

--- atcoder/dp/H.cpp
+++ atcoder/dp/H.cpp
@@ -1,28 +1,87 @@
-#include<bits/stdc++.h>
+//#pragma GCC optimize("Ofast")
+//#pragma GCC optimize ("unroll-loops")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <bits/stdc++.h>
 
 using namespace std;
 #define ll long long
-const int N = 1003;
-ll dp[N][N];
-char G[N][N];
+#define Silver_is_better_than_Gold ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define endl       '\n'
+#define return0 return 0
+#define Endl endl
+#define ull unsigned long long
+#define elif else if
+#define test   cout<<" We Droped up Here Boss _________________ "<<endl
+#define allout(v) for(auto ele : v){cout<<ele<<" ";}cout<<endl
+#define mk make_pair
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pcc pair<char,char>
+#define vi vector<int>
+#define vl vector<ll>
+#define vvi vector<vector<int>>
+#define vvl vector<vector<ll>>
+#define txt freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+#define ld long double
+#define vb vector<bool>
+#define getlost {cout<<"NO"<<endl;return ;}
+#define getgud {cout<<"YES"<<endl;return ;}
+#define tests(x) for(int i =  1 ; i<=x;i++)
+#define inp(v,n) for(int i = 0 ;  i<n;i++)cin>>v[i];
+#define outp(v) for(auto ele : v){        cout << ele.first << " " << ele.second << " ";}cout<<endl
+const ll mod = 1e9 + 7;
+#define summ(v,sum) for(auto ele : v)sum+=ele;
+#define inti int i
+#define sz(x) ((int) (x).size())
+#define dups(divs)     divs.erase(unique(divs.begin(), divs.end()), divs.end());
+#define fo(i,a,b) for(int i = a ; i<=b;i++)
+#define outq(q)     queue<int>qq = q;while (qq.size()) { cout << qq.front() << " "; qq.pop();};
+#define sttk(x) static_cast<ll>(x)
+#define re register
+#define intj int j
+#define debug cout<<"Output is  :  ";
+#define debug2 cout<<"End of the output______________________"<<endl;
+int ways = 0;
+int alpha = 1;
 int n, m;
-const ll mod = 1e9+7;
 bool valid(int x, int y) {
-    return x >= 1 and y >= 1 and x <= n and y <= m and G[x][y] == '.';
+    return (x >= 0 and y >= 0 and x < n and y < n);
 }
-ll solve(int i, int j) {
-    if (dp[i][j]!=INT_MIN)return dp[i][j];
-    return dp[i][j] = ((valid(i - 1, j) ? (solve(i - 1, j))%mod : 0) + (valid(i, j - 1) ? (solve(i, j - 1))%mod : 0))%mod;
-}
-int main() {
+    ll dp[1003][1003];
+void solve() {
     cin >> n >> m;
+    vector<vector<bool>>v(n+33, vector<bool>(m+33, 0));
+            char x;
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= m; j++) {
-            cin >> G[i][j];
-            dp[i][j]=INT_MIN;
+            cin >> x;
+            if (x == '#')v[i][j] = 1;
+            else
+                v[i][j] = 0;
         }
     }
-    dp[1][1]=1;
-    ll x= solve(n,m);
-    cout<<x;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (v[i][j] == 1)continue;
+            if (i == 1 and j == 1)dp[i][j] = 1;
+            else {
+                dp[i][j] = (dp[i-1][j]+dp[i][j-1]) % mod;
+            }
+        }
+    }
+    cout << dp[n][m] % mod;
+}
+int main() {
+    Silver_is_better_than_Gold;
+    // For getting input from input.txt file
+    //freopen("mosalah.in", "r", stdin);
+    int t = 1;
+    //  cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    //-
 }
