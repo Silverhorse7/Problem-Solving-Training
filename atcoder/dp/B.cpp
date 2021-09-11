4d40a9afb73416025698b3791c26a10ca099f898
Yosef Mahmoud <yosefone212@gmail.com>
2021-09-11 13:38:00 +0200

    Add solution for problem `B - Frog 2` Link: https://atcoder.jp/contests/dp/tasks/dp_b Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/25741728

--- atcoder/dp/B.cpp
+++ atcoder/dp/B.cpp
@@ -1,27 +1,20 @@
 #include<bits/stdc++.h>
-
 using namespace std;
-#define ll long long
-//dp[i]=max(dp[i],solve(i-1-> k);
-const int N = 1e5 + 5;
-int k;
-ll h[N], dp[N];
-
-ll solve(ll i) {
-    if (i <= 0)return INT_MAX;
-    if (i == 1)return 0;
-    if (dp[i] != INT_MAX)return dp[i];
-    for (int j = 1; j <= k; j++) {
-        if (i - j <= 0)break;
-        dp[i] = min(dp[i], solve(i - j) + abs(h[i] - h[i-j]));
+    vector<long long int>v,dp;
+    int k;
+    long long int solve(int i){
+        if(i==0)return 0;
+        if(~dp[i])return dp[i];
+        dp[i]=INT_MAX;
+        for(int j = i-1 ;j>=i-k and j>=0;j--){
+            dp[i]=min(solve(j)+(abs(v[i]-v[j])),dp[i]);
+        }
+        return dp[i];
     }
-    return dp[i];
-}
-
-int main() {
+signed main() {
     int n;
-    cin >> n >> k;
-    for (int i = 1; i <= n; i++)cin >> h[i];
-    for(int i =1 ; i <=N;i++)dp[i]=INT_MAX;
-    cout << solve(n);
+    cin>>n>>k;
+    v.resize(n);for(auto &ele  :v)cin>>ele;
+    dp.resize(n+123,-1);
+    cout<<solve(n-1);
 }
