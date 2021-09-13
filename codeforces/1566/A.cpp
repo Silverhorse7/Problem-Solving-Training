92d78d840dd13ecf874037c3d391507a0592593e
YosefMahmoud <yosefone212@gmail.com>
2021-09-13 11:20:00 +0200

    Add solution for problem `A - Median Maximization` Link: https://codeforces.com/contest/1566/problem/A Tags: binary search, greedy, math, *800 Ref: https://codeforces.com/contest/1566/submission/128711507

---
+++ codeforces/1566/A.cpp
@@ -0,0 +1,11 @@
+#include "bits/stdc++.h"
+using namespace std;
+signed main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b;
+        cin>>a>>b;
+        cout<<b/((a+2)/2)<<endl;
+    }
+}
