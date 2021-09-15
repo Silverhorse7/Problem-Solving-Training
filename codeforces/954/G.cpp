ae13160ef8aa25e1d974c273467c25e6c973dfbe
YosefMahmoud <yosefone212@gmail.com>
2021-09-15 22:01:00 +0200

    Add solution for problem `G - Castle Defense` Link: https://codeforces.com/contest/954/problem/G Tags: binary search, data structures, greedy, two pointers, *2000 Ref: https://codeforces.com/contest/954/submission/128936281

--- codeforces/954/G.cpp
+++ codeforces/954/G.cpp
@@ -14,29 +14,17 @@ bool check(ll x, vector<ll> &cur_pow) {
     for (int i = 0; i < n; i++) {
         if (x > sum + cur_pow[i]) {
             ll add = x - (sum + cur_pow[i]);
+            if(add>0) {
                 moves += add;
                 sum += add;
                 int toAddTheValue = min(i + r * 2, n - 1);
                 diff[toAddTheValue] -= add;
+            }
         }
         sum += diff[i];
         if (moves > k) return false;
     }
     if (moves <= k) return true;
-    else return false;
-}
-bool chk(ll x,vector<ll>cur_pow){
-    ll moves=0;
-    for (int i=0; i<n; i++)
-    {
-        if (x-cur_pow[i]>0)
-        {
-            ll add=  x-cur_pow[i];
-            moves+=add;
-        }
-        if (moves>k) return false;
-    }
-    if (moves<=k) return true;
     else return false;
 }
 int main() {
@@ -53,22 +41,7 @@ int main() {
 
     for (int i = 0; i < n; i++)
         cur_pow[i] = (prefix[min(i + r, n - 1)] - a[i]) - (i - r - 1 >= 0 ? prefix[i - r - 1] : 0) + a[i];
-    if(r==0){
-        long long int start = 0;
-        long long int end = 2 * 1e18;
-        long long int answer = 0;
-        while (start <= end) {
-            long long mid = (start + end) / 2;
 
-            if (chk(mid, cur_pow)) {
-                answer = mid;
-                start = mid + 1;
-            } else
-                end = mid - 1;
-        }
-        cout << answer << endl;
-        return 0;
-    }
     long long int start = 0;
     long long int end = 2 * 1e18;
     long long int answer = 0;
