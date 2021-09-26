e54361e848113b0130139e832f1333bea76d9409
YosefMahmoud <yosefone212@gmail.com>
2021-09-26 06:41:00 +0200

    Add solution for problem `C - Gargari and Bishops` Link: https://codeforces.com/contest/463/problem/C Tags: greedy, hashing, implementation, *1900 Ref: https://codeforces.com/contest/463/submission/129895176

--- codeforces/463/C.cpp
+++ codeforces/463/C.cpp
@@ -2,13 +2,12 @@
 
 #define ll long long
 using namespace std;
-const int N = 2020 * 2;
+const int N = 2000*2+1234;
 long long d1[N], d2[N];
 
 int main() {
     ios_base::sync_with_stdio();
     cout.tie();
-  //  cin.tie();
     int n;
     cin >> n;
     vector<vector<ll>> v(n, vector<ll>(n));
@@ -20,13 +19,13 @@ int main() {
             d2[i - j + n] += v[i][j];
         }
     }
-    tuple<ll, ll, ll> ans1, ans2, a = {0, 0, 0}, b = {0, 0, 0};
+    tuple<ll, ll, ll> ans1, ans2,a={0,0,0}, b={0,0,0};
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             if ((i + j) % 2 == 0)
-                ans1 = {d1[i + j] + d2[i - j + n] - v[i][j], i + 1, j + 1}, a = max(a, ans1);
+                ans1={d1[i + j] + d2[i - j + n] - v[i][j], i + 1, j + 1},a=max(a,ans1);
             else
-                ans2 = {d1[i + j] + d2[i - j + n] - v[i][j], i + 1, j + 1}, b = max(b, ans2);
+                ans2={d1[i + j] + d2[i - j + n] - v[i][j], i + 1, j + 1},b=max(b,ans2);
         }
     }
     cout << get<0>(a) + get<0>(b) << endl << get<1>(a) << ' ' << get<2>(a) << ' ' << get<1>(b) << ' ' << get<2>(b);
