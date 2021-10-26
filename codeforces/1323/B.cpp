8458f929b50e36ffc207e1df8730546722acbd30
YosefMahmoud <yosefone212@gmail.com>
2021-10-26 23:14:00 +0200

    Add solution for problem `B - Count Subrectangles` Link: https://codeforces.com/contest/1323/problem/B Tags: binary search, greedy, implementation, *1500 Ref: https://codeforces.com/contest/1323/submission/133200732

--- codeforces/1323/B.cpp
+++ codeforces/1323/B.cpp
@@ -5,7 +5,6 @@ using namespace std;
 #define vi vector<int>
 #define vl vector<ll>
 #define q(v, n) for(ll i = 0 ;  i <n;i++)cin>>v[i];
-#define q1(v, n) for(ll i = 1 ;  i <=n;i++)cin>>v[i];
 #define out(v) {for(auto &a : v)cout<<a<<' ';cout<<endl;}
 #define re resize
 #define all(v) v.begin(),v.end()
@@ -17,35 +16,61 @@ using namespace std;
 #define inti ll i
 #define intj ll j
 #define OR or
-const int N =4e5+69;
-ll a[N],b[N],pa[N],pb[N],n,m,k;
-ll calc(ll val,ll pb[],int l){
-    int cnt = 0 ;
-    for(int i =val;i<=l;i++){
-        if(pb[i]-pb[i-val] ==val)cnt++;
-    }
-    return cnt;
+const ll N = 4e4 + 5, INF = 1e9 + 5, mod = 1e9 + 7;
+
+int n, m, k, a[N], b[N];
+ll ans;
+
+set<int> getDivisors(){
+    set<int> ret;
+
+    for(int i = 1; i * i <= k; i++)
+        if(k % i == 0)
+            ret.insert(i);
+
+    return ret;
 }
-signed main(){
-    cin>>n>>m>>k;
-    q1(a,n);
-    q1(b,m);
-    for(int i =1 ; i <=n;i++)pa[i]= pa[i - 1] + a[i];
-    for(int i =1 ; i <=m;i++)pb[i]= pb[i - 1] + b[i];
-    set<ll>divs;
-    for(ll i = 1 ; i*i<=k;i++){
-        if(k%i==0){
-            divs.insert(i);
-        }
+
+int cnt(bool arr, int y){
+    int ret = 0;
+
+    if(arr)
+        for(int i = y; i <= n; i++)
+            ret += (a[i] - a[i - y] == y);
+    else
+        for(int i = y; i <= m; i++)
+            ret += (b[i] - b[i - y] == y);
+
+    return ret;
+}
+
+void doWork(){
+    cin >> n >> m >> k;
+
+    for(int i = 1; i <= n; i++)
+        cin >> a[i], a[i] += a[i - 1];
+
+    for(int j = 1; j <= m; j++)
+        cin >> b[j], b[j] += b[j - 1];
+
+    set<int> divisors = getDivisors();
+
+    for(auto& d : divisors){
+        int complement = k / d;
+
+        ans += 1LL * cnt(1, d) * cnt(0, complement);
+        if(d != complement)
+            ans += 1LL * (cnt(1, complement) * cnt(0, d));
     }
-    ll ans=  0 ;
-    for(auto ele : divs){
-        ll val=k/ele;
-        if(val==ele)ans+=calc(val,pa,n)*calc(ele,pb,m);
-        else{
-            ans+=calc(val,pb,m)*calc(ele,pa,n);
-            ans+=calc(val,pa,n)*calc(ele,pb,m);
-        }
-    }
-    cout<<ans<<endl;
+
+    cout << ans << endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+
+    int test=1;
+    //cin >> test;
+    while(test--)
+        doWork();
 }
