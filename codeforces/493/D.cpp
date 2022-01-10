9fa9322c43769fae07866767f34e7b0f66aa3767
Yosef Mahmoud <yosefone212@gmail.com>
2022-01-11 01:13:00 +0200

    Add solution for problem `D - Vasya and Chess` Link: https://codeforces.com/contest/493/problem/D Tags: constructive algorithms, games, math, *1700 Ref: https://codeforces.com/contest/493/submission/142312663

---
+++ codeforces/493/D.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    //white at (1,1) , black at (1,n)
+    /*
+     * |W
+     * |
+     * |    B
+     */
+    int n;
+    cin>>n;
+    if(n%2)cout<<"black";
+    else
+        cout<<"white"<<endl<<"1 2"<<endl;
+}
