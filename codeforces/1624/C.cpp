22e38b18d57afa80f339e8c469749c7e516b2903
Yosef Mahmoud <yosefone212@gmail.com>
2022-01-10 23:02:00 +0200

    Add solution for problem `C - Division by Two and Permutation` Link: https://codeforces.com/contest/1624/problem/C Tags: constructive algorithms, flows, graph matchings, greedy, math, *1100 Ref: https://codeforces.com/contest/1624/submission/142309183

--- codeforces/1624/C.cpp
+++ codeforces/1624/C.cpp
@@ -1,71 +1,177 @@
-#include<iostream>
-#include<vector>
-#include<set>
-#include<algorithm>
-#include<map>
+#include <bits/stdc++.h>
+
 using namespace std;
-#define int long long
-#define sz(v) (int)v.size()
-signed main() {//
-    int t;
+#define ll long long
+#define INF 1000000
+struct MaximumHelper {
+    struct Edge {
+        int to;
+        ll remain;
+        Edge *flipped;
+    };
+    const int n;
+    vector<map<int, ll>> tmp_graph;
+    vector<vector<Edge>> graph;
+    vector<vector<Edge *>> level_graph;
+    vector<int> dist, q;
+    vector<int> dead, vis_list;
+    MaximumHelper(int n_) : n(n_), tmp_graph(n), graph(n), level_graph(n), dist(n, -1), dead(n) {
+        q.reserve(n);
+        vis_list.reserve(2 * n);
+    }
+    void connect(int i, int j, ll w) {
+        tmp_graph[i][j] += w;
+        tmp_graph[j][i] += 0;
+    }
+    void init_graph() {
+        for (int i = 0; i < n; ++i)
+            graph[i].resize(tmp_graph[i].size());
+        vector<int> sz(n);
+        for (int i = 0; i < n; ++i) {
+            for (const auto &[j, w]: tmp_graph[i]) {
+                if (i > j) continue;
+                Edge &ij = graph[i][sz[i]++];
+                Edge &ji = graph[j][sz[j]++];
+                ij.to = j;
+                ji.to = i;
+                ij.remain = w;
+                ji.remain = tmp_graph[j][i];
+                ij.flipped = &ji;
+                ji.flipped = &ij;
+            }
+        }
+        for (int i = 0; i < n; ++i)
+            level_graph[i].reserve(graph[i].size());
+    }
+    bool init_level_graph() {
+        for (int i: q) {
+            dist[i] = -1;
+            level_graph[i].clear();
+        }
+        q.clear();
+        q.push_back(0);
+        dist[0] = 0;
+        for (int qq = 0; qq < (int) q.size(); ++qq) {
+            int i = q[qq];
+            for (const Edge &e: graph[i]) {
+                if (!e.remain) continue;
+                if (dist[e.to] != -1) continue;
+                dist[e.to] = dist[i] + 1;
+                q.push_back(e.to);
+                if (e.to == n - 1) break;
+            }
+            if (dist[n - 1] != -1) break;
+        }
+        if (dist[n - 1] == -1) return false;
+        for (int i: q) {
+            for (Edge &e: graph[i]) {
+                if (!e.remain) continue;
+                if (dist[i] >= dist[e.to]) continue;
+                if (e.to != n - 1 && dist[e.to] >= dist[n - 1]) continue;
+                level_graph[i].push_back(&e);
+            }
+        }
+
+        return true;
+    }
+    ll dfs(int cur, ll mn) {
+        vis_list.push_back(cur);
+        if (cur == n - 1) return INF;
+        for (int I = (int) level_graph[cur].size() - 1; I >= 0; --I) {
+            Edge *e = level_graph[cur][I];
+            if (!e->remain || dead[e->to]) {
+                level_graph[cur].pop_back();
+                continue;
+            }
+            ll nxt = min(mn, dfs(e->to, min(e->remain, mn)));
+            if (nxt) {
+                e->remain -= nxt;
+                e->flipped->remain += nxt;
+                if (!e->remain) {
+                    level_graph[cur].pop_back();
+                    if (level_graph[cur].size() == 0)
+                        dead[cur] = 1;
+                }
+                return nxt;
+            }
+            level_graph[cur].pop_back();
+        }
+        dead[cur] = 1;
+        return 0;
+    }
+    ll blocking_flow() {
+        ll ans = 0;
+        ll mn = dfs(0, INF);
+        while (mn) {
+            ans += mn;
+            mn = dfs(0, INF);
+        }
+        for (int i: vis_list)
+            dead[i] = 0;
+        vis_list.clear();
+        return ans;
+    }
+    ll solve() {
+        init_graph();
+        ll ans = 0;
+        while (init_level_graph())
+            ans += blocking_flow();
+        return ans;
+    }
+};
+
+struct MaximumMatching {
+    int n, m;
+    MaximumHelper dinics;
+    MaximumMatching(int n_, int m_) : n(n_), m(m_), dinics(2 + n + m) {
+        for (int i = 0; i < n; ++i)
+            dinics.connect(0, i + 1, 1);
+        for (int j = 0; j < m; ++j)
+            dinics.connect(j + 1 + n, 1 + n + m, 1);
+    }
+    void connect(int i, int j) {
+        dinics.connect(i + 1, j + 1 + n, 1);
+    }
+    int solve() {
+        return (int) dinics.solve();
+    }
+    vector<array<int, 2>> get_matched() {
+        solve();
+        vector<array<int, 2>> res;
+        for (int i = 0; i < n; ++i) {
+            for (auto &e: dinics.graph[i + 1]) {
+                if (e.remain) continue;
+                if (e.to < 1 + n) continue;
+                if (e.to >= 1 + n + m) continue;
+                res.push_back({i, e.to - 1 - n});
+            }
+        }
+        return res;
+    }
+};
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
     cin >> t;
     while (t--) {
         int n;
         cin >> n;
-        vector<int> v(n+44);
-        for (int i = 0; i < n; i++)cin >> v[i];
-        auto get_all = [&](int x) -> vector<int> {
-            vector<int> y;
+        MaximumMatching mm(n, n);
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
             while (x) {
-                if(x<=n)
-                    y.push_back(x);
+                if (x <= n)
+                    mm.connect(i, x - 1);
                 x /= 2;
             }
-            sort(y.begin(), y.end());
-            return y;
-        };
-        vector<vector<int>> all;
-        sort(v.begin(), v.begin()+n);
-        for (int i = 0; i < n; i++) {
-            all.push_back(get_all(v[i]));
         }
-        sort(all.begin(), all.end());
-        map<int, int> frq;
-        for (const auto& i: all)
-            for (auto j: i) {
-                frq[j]++;
-            }
-        multiset<int> ms;
-        auto cmp = [&](int &x, int &y) -> bool {
-            if (frq[x] > frq[y])return true;
-            if (frq[x] < frq[y])return false;
-            return true;
-        };
-        auto cmp1 = [&](vector<int> &v1, vector<int> &v2) -> bool {
-            if (sz(v1) > sz(v2))return 1;
-            if (sz(v1) < sz(v2))return 0;
-            for (int i = 0; i <sz(v1); i++) {
-                if (frq[v1[i]] > frq[v2[i]])return true;
-                if (frq[v1[i]] < frq[v2[i]])
-                    return false;
-            }
-            return true;
-        };
-        for (auto &i: all) {
-            sort(i.rbegin(), i.rend(), cmp);
-        }
-        sort(all.begin(), all.end(), cmp1);
-        for (auto &i: all) {
-            for (auto j: i) {
-                if (j <= n && ms.find(j) == ms.end()) {
-                    ms.insert(j);
-                    break;
-                }
-            }
-        }
-        if (ms.size() == n)cout << "YES";
+        if(mm.solve()==n)cout<<"YES"<<endl;
         else
-            cout << "NO";
-        cout << endl;
-    }//
+            cout<<"No"<<endl;
+    }
+
 }
