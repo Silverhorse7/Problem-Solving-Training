9823e6f9c400b3cdabe6cac7d5a9a15f0c80cc1c
Yosef Mahmoud <yosefone212@gmail.com>
2022-01-27 14:07:00 +0200

    Add solution for problem `D - Prefix K-th Max` Link: https://atcoder.jp/contests/abc234/tasks/abc234_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc234/submissions/28826539

--- atcoder/abc234/D.cpp
+++ atcoder/abc234/D.cpp
@@ -1,8 +1,19 @@
 #include<bits/stdc++.h>
+
 using namespace std;
+
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
+
 using namespace __gnu_pbds;
 tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> os;
-int n, k, i = 0, x;
-main() {cin >> n >> k;while (i < n) {cin >> x;os.insert(x);if (i++ >= k - 1) cout << *(os.find_by_order(os.size() - k)) << endl;}}
+int n, k, i, x;
+
+main() {
+    cin >> n >> k;
+    for (i = 0; i < n; i++) {
+        cin >> x;
+        os.insert(x);
+        if (i >= k - 1) cout << *(os.find_by_order(os.size() - k)) << endl;
+    }
+}
