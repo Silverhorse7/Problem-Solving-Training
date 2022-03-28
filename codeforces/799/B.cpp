01964e383b899cb87a9957ed1800eb2e6379b1a1
YosefMahmoud <yosefone212@gmail.com>
2022-03-28 22:53:00 +0200

    Add solution for problem `B - T-shirt buying` Link: https://codeforces.com/contest/799/problem/B Tags: data structures, implementation, *1400 Ref: https://codeforces.com/contest/799/submission/151280090

---
+++ codeforces/799/B.cpp
@@ -0,0 +1,158 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC optimize ("unroll-loops")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+#define Silver_is_better_than_Gold ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define endl       '\n'
+#define return0 return 0
+#define Endl endl
+#define ull unsigned long long
+#define elif else if
+#define test   cout<<" We Droped up Here Boss _________________ "<<endl
+#define allout(v) for(auto ele : v){cout<<ele<<" ";}cout<<endl
+#define mk make_pair
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pcc pair<char,char>
+#define vi vector<int>
+#define vl vector<ll>
+#define vvi vector<vector<int>>
+#define vvl vector<vector<ll>>
+#define txt freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+#define ld long double
+#define vb vector<bool>
+#define no {cout<<"NO"<<endl;return ;}
+#define yes {cout<<"YES"<<endl;return ;}
+#define tests(x) for(int i =  1 ; i<=x;i++)
+#define inp(v, n) for(int i = 0 ;  i<n;i++)cin>>v[i];
+#define outp(v) for(auto ele : v){        cout << ele.first << " " << ele.second << " ";}cout<<endl
+const ll mod = 1e9 + 7;
+#define inti int i
+#define sz(x) ((int) (x).size())
+#define dups(divs)     divs.erase(unique(divs.begin(), divs.end()), divs.end());
+#define fo(i, v, b) for(int i = v ; i<=b;i++)
+#define outq(q)     queue<int>qq = q;while (qq.size()) { cout << qq.front() << " "; qq.pop();};
+#define sttk(x) static_cast<ll>(x)
+#define re register
+#define intj int j
+#define debug cout<<"Output is  :  ";
+#define debug2 cout<<"End of the output______________________"<<endl;
+#define sum(v) accumulate(v.begin(),v.end(),0ll)
+#define range(i, n) for(int i = 0 ; i <n;i++)
+#define point pair<int,int>
+#define T int XXX;cin>>XXX;while(XXX--)
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
+
+ll fastpow(ll base, ll power, ll M) {
+    if (power == 1) return base;
+    ll val = fastpow(base, power / 2, M);
+    return (power % 2 == 0) ? (val * val) % M : (((val * val) % M) * base) % M;
+}
+// Computes the inverse of n modulo m.
+// Precondition: n >= 0, m > 0 and gcd(n, m) == 1.
+// The returned value satisfies 0 <= x < m (Inverse(0, 1) = 0).
+// ACHTUNG: It must hold max(m, n) < 2**31 to avoid integer overflow.
+ll Inverse(ll n, ll m) {
+    n %= m;
+    if (n <= 1) return n; // Handles properly (n = 0, m = 1).
+    return m - ((m * Inverse(m, n) - 1) / n);
+}
+
+// Fast exponentiation modulo mod. Returns x**e modulo mod.
+// Assumptions: 0 <= x < mod
+//              mod < 2**31.
+//              0 <= e < 2**63
+ll pow(ll x, ll e, ll mod) {
+    ll res = 1;
+    for (; e >= 1; e >>= 1) {
+        if (e & 1) res = res * x % mod;
+        x = x * x % mod;
+    }
+    return res;
+}
+
+// Struct that computes x % mod faster than usual, if mod is always the same.
+// It gives a x1.8 speed up over the % operator (with mod ~ 1e9 and x large).
+// It is an implementation of the Barrett reduction, see
+//    https://en.wikipedia.org/wiki/Barrett_reduction .
+// If fast_mod is an instance of the class, then fast_mod(x) will return
+// x % mod. There are no restrictions on the values of mod and x, provided
+// that they fit in an unsigned long long (and mod != 0).
+//
+// ACHTUNG: The integer type __uint128_t must be available.
+struct FastMod {
+    unsigned long long mod;
+    unsigned long long inv;
+
+    FastMod(unsigned long long mod) : mod(mod), inv(-1ULL / mod) {}
+
+    unsigned long long operator()(unsigned long long x) {
+        unsigned long long q = (unsigned long long) ((__uint128_t(inv) * x) >> 64);
+        unsigned long long r = x - q * mod;
+        return r - mod * (r >= mod);
+    }
+};
+
+//----------------------Code Starts Here-----------------------------//
+
+struct DSU {
+    vector<int> par;
+
+    DSU(int N) {
+        par.resize(N);
+        iota(all(par), 0);
+    }
+
+    int get(int x) {
+        if (par[x] == x) return x;
+        return par[x] = get(par[x]);
+    }
+
+    void unite(int x, int y) {
+        x = get(x);
+        y = get(y);
+        if (x == y) return;
+        par[x] = y;
+    }
+
+    bool connected(int u, int v) {
+        return get(u) == get(v);
+    }
+};
+
+void solve() {
+    int n;
+    cin >> n;
+    vi p(n), a(n), b(n);
+    inp(p, n);
+    inp(a, n);
+    inp(b, n);
+    multiset<pair<int, int>> st[5];
+    for (int i = 0; i < n; i++) {
+        st[a[i]].insert({p[i], i});
+        st[b[i]].insert({p[i], i});
+    }
+    int m;
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+        int c;
+        cin >> c;
+        if (st[c].empty())cout << -1 << ' ';
+        else {
+            int ind = st[c].begin()->second;
+            cout << p[ind] << ' ';
+            st[a[ind]].erase({p[ind],ind});
+            st[b[ind]].erase({p[ind],ind});
+        }
+    }
+}
+
+signed main() {
+    solve();
+}
