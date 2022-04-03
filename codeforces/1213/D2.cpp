b33631108384a4f60142659c009f14dc83869658
YosefMahmoud <yosefone212@gmail.com>
2022-04-03 20:40:00 +0200

    Add solution for problem `D2 - Equalizing by Division (hard version)` Link: https://codeforces.com/contest/1213/problem/D2 Tags: brute force, math, sortings, *1600 Ref: https://codeforces.com/contest/1213/submission/152609552

--- codeforces/1213/D2.cpp
+++ codeforces/1213/D2.cpp
@@ -1,52 +1,110 @@
-#include <bits/stdc++.h>
+//#pragma GCC optimize("Ofast")
+//#pragma GCC optimize ("unroll-loops")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include "bits/stdc++.h"
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
-typedef long long ll;
-typedef long double ld;
-const ll inf = 1e18;
-const int mod = 1e9 + 7;
-const int N = 2e5+5;
+using namespace __gnu_pbds;
+template<class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,
+        tree_order_statistics_node_update>;
+using namespace std;
+#define ll long long
+#define Silver_is_better_than_Gold ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(x) x.begin(),x.end()
+#define allr(x) x.rbegin(),x.rend()
+#define pb push_back
+#define endl       '\n'
+#define return0 return 0
+#define Endl endl
+#define ull unsigned long long
+#define elif else if
+#define test   cout<<" We Droped up Here Boss _________________ "<<endl
+#define allout(v) for(auto ele : v){cout<<ele<<" ";}cout<<endl
+#define mk make_pair
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pcc pair<char,char>
+#define vi vector<int>
+#define vl vector<ll>
+#define vvi vector<vector<int>>
+#define vvl vector<vector<ll>>
+#define txt freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+#define ld long D
+#define vb vector<bool>
+#define no {cout<<"NO"<<endl;return ;}
+#define yes {cout<<"YES"<<endl;return ;}
+#define tests(x) for(int i =  1 ; i<=x;i++)
+#define in(v, n) for(int i = 0 ;  i<n;i++)cin>>i;
+#define outp(v) for(auto ele : v){        cout << ele.first << " " << ele.S << " ";}cout<<endl
+const ll mod = 1e9 + 7;
+#define inti int i
+#define sz(x) ((int) (x).size())
+#define dups(divs)     divs.erase(unique(divs.begin(), divs.end()), divs.end());
+#define fo(i, v, b) for(int i = v ; i<=b;i++)
+#define outq(q)     queue<int>qq = q;while (qq.size()) { cout << qq.front() << " "; qq.pop();};
+#define sttk(x) static_cast<ll>(x)
+#define re register
+#define intj int j
+#define debug cout<<"Output is  :  ";
+#define debug2 cout<<"End of the output______________________"<<endl;
+#define sum(v) accumulate(v.begin(),v.end(),0ll)
+#define range(i, n) for(int i = 0 ; i <n;i++)
+#define point pair<int,int>
+#define T int XXX;cin>>XXX;while(XXX--)
+#define F first
+#define S second
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
 
-void testCase() {
-    
-    int n, x, k, ans = INT_MAX;
-
-    cin >> n >> k;
-
-    vector<int> v[N];
-
-    for (int i = 0; i < n; i++) {
-        int cnt = 0;
-        cin >> x;
-
-        while (x > 0) {
-            v[x].push_back(cnt);
-            x/=2;
-            cnt++;
-            if (x == 0)
-                v[x].push_back(cnt);
-        }
-    }
-
-    for (int i = 0; i < N; i++) {
-        if (v[i].size() < k)
-            continue;
-        sort(v[i].begin(), v[i].end());
-        ans = min(ans, accumulate(v[i].begin(), v[i].begin()+k, 0));
-    }
-
-    cout << ans;
+template<class A>
+istream &operator>>(istream &cin, vector<A> &p) {
+    for (int i = 0; i < sz(p) - 1; i++)
+        cin >> p[i];
+    return cin >> p.back();
 }
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
 
-    int testCases = 1;
+template<class A>
+ostream &operator<<(ostream &cout, vector<A> const &v) {
+    allout(v);
+    return cout;
+}
 
-   // cin >> testCases;
+ll fastpow(ll base, ll power, ll M) {
+    if (power == 1) return base;
+    ll val = fastpow(base, power / 2, M);
+    return (power % 2 == 0) ? (val * val) % M : (((val * val) % M) * base) % M;
+}
 
-    while (testCases--)
-        testCase();
-
-    return 0;
+//----------------------Code Starts Here-----------------------------//
+signed main() {
+    map<int, vector<int>> M;
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+        int cnt = 0;
+        int x = v[i];
+        while (x)
+            M[x].push_back(cnt++), x /= 2;
+    }
+    for (auto &i: M) {
+        sort(all(i.second));
+        for (int j = 1; j < i.second.size(); j++)
+            i.S[j] += i.S[j - 1];
+    }
+    int mx = INT_MAX;
+    for (int i = 0; i < n; i++) {
+        while (v[i]) {
+            if (M[v[i]].size() >= k) {
+                mx = min(mx, M[v[i]][k - 1]);
+            }
+            v[i] /= 2;
+        }
+    }
+    cout << mx << endl;
 }
