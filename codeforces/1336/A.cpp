57c839abe2c99dd8e3f8d6b7735ee859a407db21
YosefMahmoud <yosefone212@gmail.com>
2022-04-23 21:41:00 +0200

    Add solution for problem `A - Linova and Kingdom` Link: https://codeforces.com/contest/1336/problem/A Tags: dfs and similar, dp, greedy, sortings, trees, *1600 Ref: https://codeforces.com/contest/1336/submission/154784819

--- codeforces/1336/A.cpp
+++ codeforces/1336/A.cpp
@@ -1,28 +1,13 @@
 #include "bits/stdc++.h"
 
 using namespace std;
-#define int long long 
+#define int long long
 const int N = 5e5;
 int n, k;
-vector<int> adj[N], Dep[N];
+vector<int> adj[N];
 int happiness[N], industry[N], dep[N];
-bool vis[N];
+int vis[N];
 int cnt[N];
-
-void depth_assign(int root) { //to assign the depth of each node
-    queue<int> q;
-    q.push(root);
-    while (!q.empty()) {
-        auto x = q.front();
-        q.pop();
-        for (auto i: adj[x]) {
-            if (dep[i] == 0 and i != root) {
-                q.push(i);
-                dep[i] = dep[x] + 1;
-            }
-        }
-    }
-}
 
 void dfs(int root, int cur_score) { //to calculate the happiness of each node.
     if (vis[root])return;
@@ -33,15 +18,14 @@ void dfs(int root, int cur_score) { //to calculate the happiness of each node.
     }
 }
 
-void dfs2(int root) {//calculate
+void dfs2(int root, int d) {//calculate
     if (vis[root])return;
     vis[root] = 1;
     cnt[root] = 1;
-    bool f = 0;
+    dep[root] = d;
     for (auto i: adj[root]) {
         if (!vis[i]) {
-            dfs2(i);
-            f = 1;
+            dfs2(i, d + 1);
             cnt[root] += cnt[i];
         }
     }
@@ -49,30 +33,34 @@ void dfs2(int root) {//calculate
 
 signed main() {
     scanf("%lld%lld", &n, &k);
+
     for (int i = 1; i < n; i++) {
         int u, v;
         scanf("%lld%lld", &u, &v);
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
-    depth_assign(1);
-    dfs2(1);
-    for (auto &i: vis)i = 0;
+    dfs2(1, 0);
+
+    fill(vis, vis + N, 0);
+
     vector<pair<int, int>> v;
+
     for (int i = 1; i <= n; i++)
-        v.emplace_back(dep[i]-cnt[i], i);
-    sort(v.rbegin(),  v.rend());
-    //for (auto i: v)cout << i.second << ' ' << dep[i.second] << ' ' << cnt[i.second] << endl;
+        v.emplace_back(dep[i] - cnt[i], i);
+
+    sort(v.rbegin(), v.rend());
+
     for (int i = 0; i < k; i++)
         industry[v[i].second] = 1;
+
     dfs(1, 0);
 
     int ans = 0;
-    // for (int i = 1; i <= n; i++)cout << i << ":" << industry[i] << endl;
-    for (int i = 1; i <= n; i++) {
-        if (industry[i] == 1) {
+
+    for (int i = 1; i <= n; i++)
+        if (industry[i] == 1)
             ans += happiness[i];
-        }
-    }
+
     printf("%lld", ans);
 }
