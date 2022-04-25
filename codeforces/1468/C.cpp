4cd0d380f6a9bb089548f0e7a06b261e7f7d1299
YosefMahmoud <yosefone212@gmail.com>
2022-04-25 11:43:00 +0200

    Add solution for problem `C - Berpizza` Link: https://codeforces.com/contest/1468/problem/C Tags: data structures, implementation, *1400 Ref: https://codeforces.com/contest/1468/submission/154924205

--- codeforces/1468/C.cpp
+++ codeforces/1468/C.cpp
@@ -6,7 +6,7 @@ using namespace std;
 signed main() {
     int Q;
     cin >> Q;
-    map<int, int> mp1;  
+    map<int, int> mp1;
     map<int, set<int>,greater<>> mp2;
     int idx = 1;
     while (Q--) {
@@ -15,32 +15,22 @@ signed main() {
         if (q == 1) {
             int x;
             cin >> x;
-
             mp1[idx] = x;
-
             mp2[x].insert(idx);
-
             idx++;
         }
         if (q == 2) {
             cout << mp1.begin()->first << ' ';
-
             mp2[mp1.begin()->second].erase(mp1.begin()->first);
-
             if (mp2[mp1.begin()->second].size() == 0)
                 mp2.erase(mp1.begin()->second);
-
             mp1.erase(mp1.begin()->first);
         }
         if (q == 3) {
             cout << *mp2.begin()->second.begin() << ' ';
-
             mp1.erase(*mp2.begin()->second.begin());
-
             mp2.begin()->second.erase(*mp2.begin()->second.begin());
-
             if (mp2.begin()->second.size() == 0)
-
                 mp2.erase(mp2.begin());
         }
     }
