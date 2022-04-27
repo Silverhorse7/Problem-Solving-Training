d70c86251049eab9896813007934eae785a0ed20
YosefMahmoud <yosefone212@gmail.com>
2022-04-27 19:35:00 +0200

    Add solution for problem `B - Nastia and a Good Array` Link: https://codeforces.com/contest/1521/problem/B Tags: constructive algorithms, math, number theory, *1300 Ref: https://codeforces.com/contest/1521/submission/155143634

--- codeforces/1521/B.cpp
+++ codeforces/1521/B.cpp
@@ -1,6 +1,19 @@
 #include<bits/stdc++.h>
 
 using namespace std;
+#define int long long
+
+int nextPowerOf2(int n) {
+    n--;
+    n |= n >> 1;
+    n |= n >> 2;
+    n |= n >> 4;
+    n |= n >> 8;
+    n |= n >> 16;
+    n++;
+    return n;
+}
+
 int po[2] = {1000000007, 1000000009};
 
 signed main() {
@@ -16,14 +29,15 @@ signed main() {
         vector<tuple<int, int, int, int>> ans;
         for (int i = 0; i < n - 1; i++) {
             if (v[i] <= v[i + 1]) {
-                ans.emplace_back(i + 1, i + 2, v[i], v[i+1]=po[xo++]);
-                xo %= 2;
+                ans.push_back({i + 1, i + 2, v[i], v[i] + 1});
+                v[i + 1] = v[i] + 1;
             } else {
-                ans.emplace_back(i + 1, i + 2, v[i]=po[xo++], v[i + 1]);
-                xo %= 2;
+                ans.push_back({i + 1, i + 2, po[xo], v[i + 1]}), v[i] = po[xo++];
+                if (xo == 2)
+                    xo = 0;
             }
         }
-        // for (auto i: v)cout << i << ' ';
+       // for (auto i: v)cout << i << ' ';
         cout << ans.size() << endl;
         for (auto [a, b, c, d]: ans)
             cout << a << ' ' << b << ' ' << c << ' ' << d << endl;
