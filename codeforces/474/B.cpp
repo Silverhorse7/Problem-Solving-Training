e5e7c0b70aea820c627745740245aabbd87910ef
yosef mahmoud <yosefone212@gmail.com>
2022-05-02 13:32:00 +0200

    Add solution for problem `B - Worms` Link: https://codeforces.com/contest/474/problem/B Tags: binary search, implementation, *1200 Ref: https://codeforces.com/contest/474/submission/155578711

--- codeforces/474/B.cpp
+++ codeforces/474/B.cpp
@@ -11,6 +11,7 @@ signed main() {
     vector<int> Pre(n);
     for (int i = 0; i < n; i++)
         Pre[i] = v[i] + (i - 1 >= 0 ? Pre[i - 1] : 0);
+    Pre.push_back(1e9 + 7);
     int Q;
     cin >> Q;
     while (Q--) {
